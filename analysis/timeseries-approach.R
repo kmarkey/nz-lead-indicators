2e12c2c7b180917b950aec3708fb3c33f76b5db8
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-08-09 15:03:54 +0530

    Time series analysis - gets similar results to ridge regression.

---
+++ analysis/timeseries-approach.R
@@ -0,0 +1,20 @@
+library(forecast)
+library(mice)
+library(tidyverse)
+library(broom)
+
+load("data/ind_data.rda")
+
+ind_data_wide2 <- subset(ind_data_wide, !is.na(gdp_growth_lag)) %>%
+  arrange(yr_num)
+ind_imp <- complete(mice(ind_data_wide2, m = 1, print = FALSE), 1)
+
+Y <- ts(ind_imp$gdp_growth, start = c(1987, 4), frequency = 4)
+X <- scale(select(ind_imp, -yr_num, -gdp_growth, -gdp_growth_lag))
+
+mod <- auto.arima(Y, xreg = X)
+
+
+broom::tidy(confint(mod))
+# these results are very similar to what we get with the ridge regression approach, but they lose in interpretability
+# (because of the moving average and seasonal moving average rather than just relying on the lagged value of growth).
